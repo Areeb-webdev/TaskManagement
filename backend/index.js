54ef024a320f00f40700c46f4f641132bf9a000a
Muhammad Areeb <137597358+Areeb-webdev@users.noreply.github.com>
2025-03-26 21:55:51 +0500

    index.js

--- backend/index.js
+++ backend/index.js
@@ -6,22 +6,11 @@ import TaskRouter from "./Routes/TaskRouter.js";
 import cors from "cors";
 
 const app = express();
-const corsOptions = {
-  origin: "https://task-management-ui-sable.vercel.app", // Your frontend URL
-  methods: "GET, POST, PUT, DELETE",
-  allowedHeaders: "Content-Type, Authorization"
-};
-
-app.use(cors(corsOptions));
-
-
 const PORT = process.env.PORT || 5000  
 connectDB();
 app.use(cors());
 app.use(express.json()); // Built-in JSON middleware
 app.use(express.urlencoded({ extended: false })); // Handle form data
-
-// console.log("DB_URL:", process.env.DB_URL);
 
 app.get("/", (req, res) => {
     res.send(`hello from server  ${PORT}`);
