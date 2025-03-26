3f3600650393694e69e763d2a073f2b917ef57de
Muhammad Areeb <137597358+Areeb-webdev@users.noreply.github.com>
2025-03-26 21:24:15 +0500

    utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,4 +2,4 @@ import { toast } from "react-toastify";
 export const notify = (message, type)=>{
  toast[type](message)
 }
-export const API_URL  = "https://task-management-api-tawny-iota.vercel.app";
+export const API_URL  = "https://task-management-api-pied.vercel.app";
